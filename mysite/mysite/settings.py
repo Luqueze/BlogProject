cc9bee1ac327487baaf906bd0302943e9e7e3271
John Doe <lucashaleluia@hotmail.com>
2025-04-29 23:24:30 -0300

    css and html added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Garante que os arquivos estáticos sejam acessíveis via /static/
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_DIRS = [BASE_DIR / "blog/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
